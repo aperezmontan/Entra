5c2adcc0157e27021fd6c9f94aef5c9e2704f226
Ari Perez <aris.a.perez@gmail.com>
2015-05-31 14:28:37 -0400

    Adds ability to view and create clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,8 +5,20 @@ class ClientsController < ApplicationController
 
   end
 
-  def create
+  def new
+    @client = Client.new
+  end
 
+  def create
+    new_client = Client.new(get_params)
+    new_client.user_id  = current_user.id
+    if new_client.save
+      flash[:success] = "#{new_client.name} was saved"
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = "This client could not be saved"
+      redirect_to :back #ask about changing this
+    end
   end
 
   def update
@@ -15,6 +27,19 @@ class ClientsController < ApplicationController
 
   def delete
 
+  end
+
+  private
+
+  def get_params
+    params.require(:client).permit(:name, :phone, :email)
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "Please sign in."
+      redirect_to login_path
+    end
   end
 
 end
