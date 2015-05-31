eec539001171028e6c41417e255a68657f7e123f
Ace Burgess <burgess.ace@gmail.com>
2015-05-31 00:03:10 -0400

    Alter before action on TwilioController

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,24 +1,28 @@
 class TwilioController < ApplicationController
-  before_action :twilio_authentication, only: [:send_text_message]
-
-  def new
-  end
+  before_action :twilio_authentication, :get_twilio_account
 
   def send_text_message
     number_to_send_to = params[:user_phone]
-    @twilio = Twilio::REST::Client.new @twilio_sid, @twilio_token
     @twilio.account.sms.messages.create(
       from: "+1#{@twilio_phone_number}",
       to: number_to_send_to,
-      body: "This is an message. It gets sent to #{number_to_send_to}"
+      body: "Hi <ENTER USER> from #{number_to_send_to}. To unlock your door. Please reply to this message."
     )
   end
 
+  def receive_text_message
+    p params
+    binding.pry
+  end
 
   private
   def twilio_authentication
     @twilio_sid = ENV['TWILIO_ACCOUNT_SID']
     @twilio_token = ENV['TWILIO_AUTH_TOKEN']
     @twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
+  end
+
+  def get_twilio_account
+    @twilio = Twilio::REST::Client.new @twilio_sid, @twilio_token
   end
 end
