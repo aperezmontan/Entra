fd87a15c9ff2757cd0616bd15ca15ec38ae8dd21
echenique11 <echenique11@gmail.com>
2015-06-01 10:55:40 -0400

    adding base url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :require_login, :time_until
+  helper_method :current_user, :logged_in?, :require_login, :time_until, :base_url
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user != nil
+  end
+
+  def base_url
+    "#{request.protocol}#{request.host_with_port}"
   end
 
   private
--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,9 +1,9 @@
 class ClientMailer < ApplicationMailer
   default from: ENV['GMAIL_ACCOUNT']
  
-  def buzzer_email(clientKey,current_user)
+  def buzzer_email(base_url,clientKey,current_user)
     @clientKey = clientKey
-    @url  = "http://10.0.2.185:3000/request_open_show/#{@clientKey.hashify}"
+    @url  = "#{base_url}/request_open_show/#{@clientKey.hashify}"
     mail(to: @clientKey.client.email, subject: "Access granted by #{current_user.first_name}")
   end
 end
