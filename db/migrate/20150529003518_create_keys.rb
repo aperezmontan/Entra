61c88dbca04b86a741a86783aab97bade485e981
Ace Burgess <burgess.ace@gmail.com>
2015-06-01 11:16:46 -0400

    Change schema for the better

--- db/migrate/20150529003518_create_keys.rb
+++ db/migrate/20150529003518_create_keys.rb
@@ -4,6 +4,10 @@ class CreateKeys < ActiveRecord::Migration
       t.references :place, null: false
       t.datetime :start_date, null: false
       t.datetime :end_date, null: false
+      t.references :guest, null: false
+      t.datetime :used_at
+      t.boolean :requested, default: false, null: false
+      t.string :secret_url, null: false
 
       t.timestamps null: false
     end
