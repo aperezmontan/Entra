4229c2647d87d6235a072d81cc0be0e82bb6af20
Ari Perez <aris.a.perez@gmail.com>
2015-06-01 03:05:47 -0400

    Adds ability to create client keys and shows upcoming reservations on user show route

--- app/models/client_key.rb
+++ app/models/client_key.rb
@@ -13,7 +13,49 @@ class ClientKey < ActiveRecord::Base
   def get_start_date_string
     key.start_date.strftime("%Y/%m/%d %H:%M:%S")
   end
-  
+
+  def time_until(time_stamp)
+    time_hash = {
+    2419201 => "a few months from now",
+    2419200 => "less than a month",
+    1209600 => "about a couple of weeks",
+    604800 => "about a week",
+    345600 => "a few days",
+    86400 => "less than a day",
+    14400 => "less than a few hours",
+    7200 => "less than 2 hours",
+    5400 => "less than an hour and a half",
+    3600 => "less than an hour",
+    2700 => "less than 45 minutes",
+    1800 => "less than 30 minutes",
+    1200 => "less than 20 minutes",
+    900 => "less than 15 minutes",
+    600 => "less than 10 minutes",
+    300 => "less than 5 minutes",
+    120 => "less than 2 minutes",
+    60 => "less than 1 minute",
+    1 => "less than 1 second"}
+
+    time_hash.each do |key,value|
+      if (time_stamp - Time.now.utc) > key
+        return value
+      end
+    end
+  end
+
+  def name
+    key = Key.find_by(id: self.key_id)
+    client = Client.find_by(id: self.client_id)
+    time = time_until(key.start_date)
+    if Time.now.utc > key.end_date
+      "#{client.name} had access to #{key.place.nick_name}"
+    elsif Time.now.utc > key.start_date && Time.now.utc < key.end_date
+      "#{client.name} currently has access to #{key.place.nick_name}"
+    else
+    "#{client.name} will have access to #{key.place.nick_name} in #{time}"
+    end
+  end
+
   private
 
   def set_hashify
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :notifications
   has_secure_password
 
+  def reservations
+    clientids = []
+    self.clients.each do |client|
+      clientids << client.id
+    end
+
+    ClientKey.where( "client_id = ?", clientids)
+  end
+
 end
