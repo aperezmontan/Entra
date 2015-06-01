d349fef79c494a549237869d0f04fbf1a86224dd
Ace Burgess <burgess.ace@gmail.com>
2015-06-01 17:10:34 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,6 @@ ari.save
 place = Place.create(address:'Juan Dolio', nickname:'Beach House',admin_id:ari.id)
 guest = Guest.create(name:'Luis', phone:'8098541988',user_id:ari.id,email:'echenique11@hotmail.com')
 key = Key.create(place_id:place.id,start_date:Time.now,end_date:Date.parse('11-11-2020'),guest_id: guest.id)
+
+ari.deafult_place = place
+ari.save
