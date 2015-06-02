86cae6b2c9285c44ba742a946dab6cca1f5484f6
echenique11 <echenique11@gmail.com>
2015-06-02 11:45:46 -0400

    showing button when countdown to availble

--- app/assets/javascripts/key_countdown.js
+++ app/assets/javascripts/key_countdown.js
@@ -1,7 +1,10 @@
 function availableIn(startDate){
   $("#available-in").countdown(startDate, function(event) {
-    $(this).text(
-      event.strftime('%D days %H:%M:%S')
-    );
+    var countDown = event.strftime('%D days %H:%M:%S');
+    if(countDown != "00 days 00:00:00"){
+      $(this).text(countDown);      
+    } else {
+      $('#lock-container').html("<%= escape_javascript(render partial: 'places/giant_lock', locals: { name: @key.guest.name, id: @key.id, place_name: @key.place.nickname, tag: 'key'}) %>");
+    }
   });
 }
