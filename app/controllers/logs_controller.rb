e73f9bfc9f0d08127d4c5034d3306b4e5588c165
Ari Perez <aris.a.perez@gmail.com>
2015-06-02 13:27:10 -0400

    Fix places controller problem

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,7 +2,7 @@ class LogsController < ApplicationController
   before_action :require_login, only: [:index]
 
   def index
-    @all_logs = Log.where(loggable_type: "Guest", loggable_id: current_user.guest_ids).order(created_at: :desc) + Log.where(loggable_type: "User", loggable_id: current_user.id).order(created_at: :desc)
+    @all_logs = Log.where(loggable_type: "User", loggable_id: current_user.id).order(created_at: :desc)
   end
 
   private
@@ -10,5 +10,7 @@ class LogsController < ApplicationController
   def notification_params
     params.permit(:user_id,:key_id,:message)
   end
+
+  def
 
 end
