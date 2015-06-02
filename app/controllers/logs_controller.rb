cde8828790ff582796ab9acb68116f8df5bebb02
Ari Perez <aris.a.perez@gmail.com>
2015-06-01 23:01:34 -0400

    Adds log display finctionality

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,8 @@
 class LogsController < ApplicationController
+  before_action :require_login, only: [:index]
 
   def index
-    all_logs = Log.where()
+    @all_logs = Log.where(loggable_type: "Guest", loggable_id: current_user.guest_ids).order(created_at: :desc) + Log.where(loggable_type: "User", loggable_id: current_user.id).order(created_at: :desc)
   end
 
   private
--- app/models/log.rb
+++ app/models/log.rb
@@ -2,83 +2,83 @@ class Log < ActiveRecord::Base
   belongs_to :loggable
 
   def new_key_success_message(key)
-   message = "SUCCESS ! You created a new key for #{key.guest.name} at #{key.place.nickname}"
+   self.message = "SUCCESS ! You created a new key for #{key.guest.name} at #{key.place.nickname}"
   end
 
   def new_key_fail_message(key)
-   message = "FAILURE :( You could not create a new key for #{key.guest.name} at #{key.place.nickname}"
+   self.message = "FAILURE :( You could not create a new key for #{key.guest.name} at #{key.place.nickname}"
   end
 
   def destroy_key_success_message(key)
-   message = "SUCCESS ! You destroyed a key for #{key.guest.name} at #{key.place.nickname}"
+   self.message = "SUCCESS ! You destroyed a key for #{key.guest.name} at #{key.place.nickname}"
   end
 
   def destroy_key_fail_message(key)
-   message = "FAILURE :( You could not destroy a key for #{key.guest.name} at #{key.place.nickname}"
+   self.message = "FAILURE :( You could not destroy a key for #{key.guest.name} at #{key.place.nickname}"
   end
 
   def new_place_success_message(place)
-   message = "SUCCESS ! You created a new place called #{place.nickname}"
+   self.message = "SUCCESS ! You created a new place called #{place.nickname}"
   end
 
   def new_place_fail_message(place)
-   message = "FAILURE :( You could not create #{place.nickname}"
+   self.message = "FAILURE :( You could not create #{place.nickname}"
   end
 
   def destroy_place_success_message(place)
-   message = "SUCCESS ! You destroyed #{place.nickname}"
+   self.message = "SUCCESS ! You destroyed #{place.nickname}"
   end
 
   def destroy_place_fail_message(place)
-   message = "FAILURE :( You could not destroy #{place.nickname}"
+   self.message = "FAILURE :( You could not destroy #{place.nickname}"
   end
 
   def new_guest_success_message(guest)
-   message = "SUCCESS ! You created a new guest called #{guest.name}"
+   self.message = "SUCCESS ! You created a new guest called #{guest.name}"
   end
 
   def new_guest_fail_message(guest)
-   message = "FAILURE :( You could not create #{guest.name}"
+   self.message = "FAILURE :( You could not create #{guest.name}"
   end
 
   def destroy_guest_success_message(guest)
-   message = "SUCCESS ! You destroyed #{guest.name}"
+   self.message = "SUCCESS ! You destroyed #{guest.name}"
   end
 
   def destroy_guest_fail_message(guest)
-   message = "FAILURE :( You could not destroy #{guest.name}"
+   self.message = "FAILURE :( You could not destroy #{guest.name}"
   end
 
   def notification_success_message(notification)
-   message = "SUCCESS ! #{guest.name} send you a notification"
+   self.message = "SUCCESS ! #{guest.name} send you a notification"
   end
 
   def notification_failure_message(notification)
-   message = "FAILURE :( #{guest.name} send you a notification"
+   self.message = "FAILURE :( #{guest.name} send you a notification"
   end
 
   def requested_open_successfully_message(key)
-   message = "SUCCESS ! #{key.guest.name} successfully requested to open #{key.place.nickname}"
+   self.message = "SUCCESS ! #{key.guest.name} successfully requested to open #{key.place.nickname}"
   end
 
   def requested_open_unsuccessfully_message(key)
-   message = "FAILURE :( #{key.guest.name} unsuccessfully requested to open #{key.place.nickname}"
+   self.message = "FAILURE :( #{key.guest.name} unsuccessfully requested to open #{key.place.nickname}"
   end
 
   def key_used_successfully_message(key)
-   message = "SUCCESS ! #{key.guest.name} successfully used a key for #{key.place.nickname}"
+   self.message = "SUCCESS ! #{key.guest.name} successfully used a key for #{key.place.nickname}"
   end
 
   def key_used_unsuccessfully_message(key)
-   message = "FAILURE :( #{key.guest.name} tried unsuccessfully to use a key for #{key.place.nickname}"
+   self.message = "FAILURE :( #{key.guest.name} tried unsuccessfully to use a key for #{key.place.nickname}"
   end
 
   def admin_use_success(place)
-   message = "SUCCESS ! You used a key for #{place.nickname}"
+   self.message = "SUCCESS ! You used a key for #{place.nickname}"
   end
 
   def admin_use_fail(place)
-   message = "FAILURE :( You used a key for #{place.nickname}"
+   self.message = "FAILURE :( You used a key for #{place.nickname}"
   end
 
   def time
