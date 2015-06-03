27b1f24fca60a96ff5d4124514361240a628ee33
echenique11 <echenique11@gmail.com>
2015-06-02 22:09:12 -0400

    sending notification link to reactivate a key

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -6,4 +6,13 @@ class GuestMailer < ApplicationMailer
     @url  = "#{base_url}/find_guests_key_by_url/#{@key.secret_url}"
     mail(to: @key.guest.email, subject: "Access granted by #{current_user.first_name}")
   end
+
+  def notification_email(key,current_user,message,base_url)
+    @message = message
+    @key = key
+    @link = base_url + "/keys/#{key.id}/edit"
+    @user_name = current_user.first_name
+    mail(to: current_user.email, subject: "Notification from #{@key.guest.name}")
+  end
+
 end
