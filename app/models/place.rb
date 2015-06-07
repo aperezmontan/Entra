d8a86673faa4ac7f4368fa45909232200e8a25ab
Ace Burgess <burgess.ace@gmail.com>
2015-06-07 17:53:09 -0400

    Write validations for model to protect database and espcially make email uniqueness required

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,8 @@ class Place < ActiveRecord::Base
   has_many :keys
   has_many :logs, as: :loggable
 
+  validates :nickname, :address, :admin_id, presence: true
+
   def available_key
     current_time = Time.new.getutc
     keys.where([" (start_date <= ? and end_date >= ? and used_at is NULL and requested = ?) or ( requested = ? and unlimited_access = ?) ",current_time,current_time,true,true,true]).take
